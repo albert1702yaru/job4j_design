9bfe24dcc724a3bbbf2e4bbace6b4acfdaa1cfeb
Альберт <albert1702@ya.ru>
2023-08-18 07:18:41 +0300

    7. ListIterator [#350217 #437468]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -7,18 +7,23 @@ public class ListUtils {
 
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (iterator.nextIndex() == index) {
-                iterator.add(value);
-                break;
-            }
-            iterator.next();
-        }
+        list.add(index, value);
+//        ListIterator<T> iterator = list.listIterator();
+//        while (iterator.hasNext()) {
+//            if (iterator.nextIndex() == index) {
+//                iterator.add(value);
+//                break;
+//            }
+//            iterator.next();
+//        }
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
-        addBefore(list, index + 1, value);
+        if (index >= list.size()) {
+            list.add(value);
+        } else {
+            list.add(index + 1, value);
+        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
@@ -41,12 +46,7 @@ public class ListUtils {
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> iterator = list.listIterator();
-        while (iterator.hasNext()) {
-            if (elements.contains(iterator.next())) {
-                iterator.remove();
-            }
-        }
+        removeIf(list, elements::contains);
     }
 
 }
